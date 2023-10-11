332caeab8210f3f37ad2c4cf82949c4b3fe11c41
Thalles Daniel <106764514+ThallesDaniel@users.noreply.github.com>
2023-10-11 09:31:25 -0300

    atualização - login no tik tok

--- WebAutomationApplication/src/main/java/com/automacao/WebAutomationApplication/WebAutomationApplication.java
+++ WebAutomationApplication/src/main/java/com/automacao/WebAutomationApplication/WebAutomationApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 @SpringBootApplication
@@ -18,7 +17,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 @EnableAsync
 public class WebAutomationApplication {
 
-	private static final String URL = "https://www.tiktok.com/pt-BR/";
+	private static final String URL = "https://www.tiktok.com/pt-BR";
 	private static final String XPATH = "//*[@id=\"main-content-homepage_hot\"]/div[1]/div[2]/div/div[2]/div[2]/button[1]/span";
 
 	public static void main(String[] args) {
@@ -33,12 +32,31 @@ public class WebAutomationApplication {
 		try {
 			driver.get(URL);
 
+			Thread.sleep(5000);
+
+			WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"loginContainer\"]/div/div/div[2]"));
+			loginButton.click();
+
+			Thread.sleep(15000);
+
+			WebElement emailInput = driver.findElement(By
+					.xpath("//*[@id=\"yDmH0d\"]/c-wiz/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div"));
+			emailInput.sendKeys("teytesss343@gmail.com");
+			Thread.sleep(5000);
+
+			WebElement passwordInput = driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
+			passwordInput.sendKeys("Corujinhaead4m4");
+
+			WebElement nextButton = driver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button/div[3]"));
+			nextButton.click();
+
+			Thread.sleep(25000);
+
 			WebElement element = driver.findElement(By.xpath(XPATH));
 			element.click();
 
-			((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 500)");
+			((JavascriptExecutor) driver).executeScript("window.scrollBy(0, 500");
 
-			Thread.sleep(30 * 60 * 1000);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -46,3 +64,4 @@ public class WebAutomationApplication {
 		}
 	}
 }
+
